ef311cfefcc301a509fa432e3ceb64d889e4009d
Maciej Brencz <maciej.brencz@gmail.com>
2013-11-19 23:07:31 +0100

    ZaKtualizuj przykłady

--- phantomjs/example/example-netsniff.js
+++ phantomjs/example/example-netsniff.js
@@ -1,16 +1,17 @@
+#!/usr/bin/env phantomjs
 var page = require('webpage').create(),
 	url = "http://akai.org.pl",
 	requests = 0;
 
 // żądanie HTTP zostało wysłane
 page.onResourceRequested = function (request) {
-	console.log('Request ' + JSON.stringify(request, undefined, 4));
+	console.log('Request sent:\n' + JSON.stringify(request, undefined, 4));
+	requests++; // zliczajmy wszystkie zapytania HTTP
 };
 
 // otrzymano odpowiedź od serwera HTTP
 page.onResourceReceived = function (response) {
-	console.log('Receive ' + JSON.stringify(response, undefined, 4));
-	requests++; // zliczajmy wszystkie zapytania HTTP
+	console.log('Received:\n' + JSON.stringify(response, undefined, 4));
 };
 
 page.open(url, function (status) {
